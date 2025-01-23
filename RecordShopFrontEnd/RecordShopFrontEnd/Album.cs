a0376d57ab83661eb925e6dffd7ee7b14b593acd
sz-99 <zhengsusan3@gmail.com>
2025-01-23 15:27:19 +0000

    connect to backend api and load albums to display

---
+++ RecordShopFrontEnd/RecordShopFrontEnd/Album.cs
@@ -0,0 +1,24 @@
+﻿namespace RecordShopFrontEnd
+{
+    public class Album
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Artist { get; set; }
+        public int ReleaseYear {  get; set; }
+        public Genre Genre { get; set; }
+
+    }
+    public enum Genre
+    {
+        Pop,
+        Rock,
+        Country,
+        RnB,
+        HipHop,
+        Jazz,
+        Classic,
+        Metal
+    }
+  
+}
---
+++ RecordShopFrontEnd/RecordShopFrontEnd/AlbumList.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+
+namespace RecordShopFrontEnd
+{
+    public class AlbumList
+    {
+        public static List<Album> Albums = new List<Album>()
+        {
+            new Album() { Id = 1, Name = "30", Artist = "Adele", ReleaseYear = 2021, Genre = Genre.Pop },
+            new Album() { Id = 2, Name = "21", Artist = "Adele", ReleaseYear = 2011, Genre = Genre.Pop },
+            new Album() { Id = 3, Name = "Hotel California", Artist = "Eagles", ReleaseYear = 1976, Genre = Genre.Country }
+        };
+    }
+}
