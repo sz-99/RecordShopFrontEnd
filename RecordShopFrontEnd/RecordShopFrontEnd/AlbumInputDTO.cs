b162559c73aa2a362699fbc65fdfaed09fee4013
sz-99 <zhengsusan3@gmail.com>
2025-01-28 08:42:56 +0000

    add form and album dto to submit new album

--- RecordShopFrontEnd/RecordShopFrontEnd/AlbumInputDTO.cs
+++ RecordShopFrontEnd/RecordShopFrontEnd/AlbumInputDTO.cs
@@ -1,33 +1,27 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 
 namespace RecordShopFrontEnd
 {
-    public class Album
+    public class AlbumInputDTO
     {
-        public int? Id { get; set; }
         [Required]
         [StringLength(100, ErrorMessage = "Name cannot exceed 100 characters.")]
+        [JsonPropertyName("name")]
         public string Name { get; set; } = string.Empty;
         [Required]
         [StringLength(100, ErrorMessage = "Artist Name cannot exceed 100 characters.")]
+        [JsonPropertyName("artist")]
+
         public string Artist { get; set; } = string.Empty;
         [Required]
         [Range(1700, 2050, ErrorMessage = "Please enter valid year.")]
-        public int ReleaseYear {  get; set; }
-        [EnumDataType(typeof(Genre), ErrorMessage = "Please input valid Genre.")]
-        public Genre Genre { get; set; } = 0;
+        [JsonPropertyName("releaseYear")]
 
+        public int ReleaseYear { get; set; }
+        [EnumDataType(typeof(Genre), ErrorMessage = "Please input valid Genre.")]
+        [JsonPropertyName("genre")]
+
+        public Genre Genre { get; set; } = 0;
     }
-    public enum Genre
-    {
-        Pop,
-        Rock,
-        Country,
-        RnB,
-        HipHop,
-        Jazz,
-        Classic,
-        Metal
-    }
-  
 }
